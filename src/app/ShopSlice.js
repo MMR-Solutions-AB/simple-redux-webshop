887ff6b38b48b3b816eaee48fa871537b701e854
Riickard <83611587+Riickard@users.noreply.github.com>
2022-08-07 21:50:56 +0200

    Reducer Change

--- src/app/ShopSlice.js
+++ src/app/ShopSlice.js
@@ -13,10 +13,26 @@ const shopSlice = createSlice({
   initialState,
   reducers: {
     addCartItem: (state, action) => {
+      /* Om ny tillagd produkt existerar i kundkorg, adderar antalet av den produkten "amount" */
+      const validateItem = state.cart.find((item) => {
+        const { title } = action.payload;
+        return item.title === title;
+      });
+
       /* Hur vi ändrar state i React & Context API, Vi byter ut det gamla mot ett nytt och uppdaterat state */
-      state.cart = [...state.cart, { ...action.payload, amount: 1 }];
+      if (!validateItem) {
+        state.cart = [...state.cart, action.payload];
+      } else {
+        state.cart = state.cart.map((item) => {
+          return item.title === validateItem.title
+            ? { ...item, amount: Number(item.amount) + 1 }
+            : item;
+        });
+      }
+      /* ---------------------------------------------------------------------- */
       /* Redux tillåter Mutation i sin reducer, tillskillnad från React useState */
       /* state.cart.push({ ...action.payload, amount: 1 }); */
+      /* ---------------------------------------------------------------------- */
     },
   },
 });
